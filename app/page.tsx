d08301d83f41e55ba239471705e1c50f5c92e24b
Kirill Grigorev <kirillisomska2@gmail.com>
2024-02-16 14:28:36 +0600

    add contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,63 @@
 import ProjectsList from "@/components/projects-list/ProjectsList";
 import { getAllProjectsWithoutCustomers } from "@/db/db";
+import Image from "next/image";
+import Link from "next/link";
+
+import tgLogo from "@/public/icons8-телеграм.svg";
+import vkLogo from "@/public/icons8-vk.svg";
 
 export default async function Home() {
   const projects = await getAllProjectsWithoutCustomers();
 
   return (
     <>
-      <div className="custom-screen max-w-[1280px] w-[100%] m-auto gap-x-20 items-center md:flex">
+      <div className="custom-screen max-w-[1280px] w-[100%] px-4 m-auto items-center flex-col gap-16">
         <ProjectsList projects={projects} />
+        <div className="flex justify-between mt-20">
+          <iframe
+            src="https://yandex.ru/map-widget/v1/?um=constructor%3A9cd4beba281a298ab562252e24fa0488dee5863cb54f450f100cbf46d83387dd&amp;source=constructor"
+            width="928"
+            height="647"
+            className="rounded-2xl max-h-[520px] max-w-[900px] w-full"
+          ></iframe>
+          <div>
+            <h2 className="text-gray-900 font-bold text-3xl mb-2 mt-3">Всегда на связи</h2>
+            <address>
+              <div>
+                <a className="text-gray-900 font-bold text-xl mb-2 mt-3" href="tel:+78008008080">8 (800) 800-80-80</a>
+                <p className="text-gray-900 mb-2">Звоните с 12:00 до 23:00 по МСК</p>
+              </div>
+              <div>
+                <a className="text-gray-900 font-bold text-xl mb-2 mt-3" href="mailto:example@gmail.com">example@gmail.com</a>
+                <p className="text-gray-900 mb-2">Пишите, когда вам удобно </p>
+              </div>
+              <div className="flex gap-3">
+                <Link href="#">
+                  <div className="flex gap-2 items-center">
+                    <Image
+                      src={tgLogo}
+                      alt="Tg-logo"
+                      width={50}
+                      height={50}
+                    ></Image>
+                    <p>Telegram</p>
+                  </div>
+                </Link>
+                <Link href="#">
+                  <div className="flex gap-2 items-center">
+                    <Image
+                      src={vkLogo}
+                      alt="Tg-logo"
+                      width={50}
+                      height={50}
+                    ></Image>
+                    <p>ВКонтакте</p>
+                  </div>
+                </Link>
+              </div>
+            </address>
+          </div>
+        </div>
       </div>
     </>
   );
--- components/projects-list/ProjectsList.tsx
+++ components/projects-list/ProjectsList.tsx
@@ -23,38 +23,49 @@ const getAllProjectsComponents = (projects: Project[]) => {
     const date = new Date(project.date);
 
     return (
-    <li className="mb-10 ms-4" key={project.id}>
-      <div className="absolute w-3 h-3 bg-blue-400 rounded-full mt-1.5 -start-1.5 border border-white"></div>
-      <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{`${
-        monthList[date.getMonth()]
-      }, ${date.getFullYear()}`}</time>
-      {project.imageUrl ? (
-        <Image
-          src={project.imageUrl}
-          width={300}
-          height={300}
-          alt="Картинка проекта"
-        />
-      ) : null}
-      <h2 className="text-lg font-semibold text-gray-900">{project.name}</h2>
-      <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-        {project.shortDescription}
-      </p>
-      <Link
-        className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-400 border border-gray-200 rounded-lg hover:bg-white hover:text-blue-400"
-        href={`/projects/${project.id}`}
-      >
-        Подробнее
-      </Link>
-    </li>)
+      <li className="mb-10 ms-4" key={project.id}>
+        <div className="absolute w-3 h-3 bg-blue-400 rounded-full mt-1.5 -start-1.5 border border-white"></div>
+        <time className="text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{`${
+          monthList[date.getMonth()]
+        }, ${date.getFullYear()}`}</time>
+        <div className="flex justify-between">
+          <div className="max-w-[60%]">
+            <h2 className="text-lg font-semibold text-gray-900">
+              {project.name}
+            </h2>
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+              {project.shortDescription}
+            </p>
+            <Link
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-400 border border-gray-200 rounded-lg hover:bg-white hover:text-blue-400"
+              href={`/projects/${project.id}`}
+            >
+              Подробнее
+            </Link>
+          </div>
+          {project.imageUrl ? (
+            <Image
+              src={project.imageUrl}
+              width={300}
+              height={300}
+              alt="Картинка проекта"
+              className="max-h-[200px] h-full object-cover max-w-[35%]"
+            />
+          ) : null}
+        </div>
+      </li>
+    );
   });
 };
 
 const ProjectsList = ({ projects }: { projects: Project[] }) => {
   return (
-    <ol className="list-style-color-red relative border-s border-blue-400">
-      {getAllProjectsComponents(projects)}
-    </ol>
+    <div className="w-full">
+      <h2 className="mt-10 mb-4 text-gray-600 font-bold text-2xl text-center">Проекты</h2>
+      <ol className="list-style-color-red relative border-s border-blue-400 max-w-[1000px] w-full mx-auto">
+        {getAllProjectsComponents(projects)}
+      </ol>
+    </div>
   );
 };
 
