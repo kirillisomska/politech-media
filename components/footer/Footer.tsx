a48c2469965978c587c8071bb8b6812c0e95aafb
Kirill Grigorev <kirillisomska2@gmail.com>
2024-03-23 23:28:15 +0600

    remove footer styles

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,23 +1,11 @@
 import logo from "@/public/logo.svg";
 import Image from "next/image";
-import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="custom-screen max-w-[1280px] w-[100%] mx-auto px-4">
       <div className="flex-1 items-center pt-8 text-gray-600 font-medium md:mt-0 flex flex-wrap gap-5 justify-center">
         <Image src={logo} alt="Логотип" width={200} height={50}></Image>
-        {/* <ul className="justify-center items-center flex space-x-6 space-y-0 flex-wrap">
-          <li className="hover:text-gray-900">
-            <Link href="/#about">О компании</Link>
-          </li>
-          <li className="hover:text-gray-900">
-            <Link href="/#projects">Проекты</Link>
-          </li>
-          <li className="hover:text-gray-900">
-            <Link href="/#contacts">Контакты</Link>
-          </li>
-        </ul> */}
       </div>
       <div>
         <p className="text-gray-600 mt-3 pb-5 text-center">{`© ${new Date().getFullYear()}, ПолитехМедиа`}</p>
